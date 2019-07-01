b6b7be289cc158d2cd5d50a0f9402f18feefbc0d
Antonio Casado <acasado@opensistemas.com>
2019-07-01 10:55:14 +0200

    Section 3: completed

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,34 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-        'Access-Control-Allow-Header',
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+        "GET, POST, PATCH, DELETE, OPTIONS"
     );
     next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(200).json({
+        message: 'Post added succesfully'
+    });
+});
+
+app.get('/api/posts', (req, res, next) => {
     const posts = [
         {
             id: 'khsaf222',
