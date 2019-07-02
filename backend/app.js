d17ee1611727bbe17bc9165eb5dcafa8168a3b74
Antonio Casado <acasado@opensistemas.com>
2019-07-02 12:43:21 +0200

    Section 6: completed

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
@@ -17,6 +18,7 @@ mongoose.connect("mongodb://localhost:27017/udemy-mean", {useNewUrlParser: true}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static( path.join('backend/images')));
 
 
 app.use((req, res, next) => {
